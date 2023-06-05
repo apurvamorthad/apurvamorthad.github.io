7879fe4432a221275018ca5aee4aabff1d01d588
apurvamorthad <93441698+apurvamorthad@users.noreply.github.com>
2023-06-05 21:48:22 +0530

    performed DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 alert("Hai, I'm Apurva");
+var user1 = {image : "childpic.jpg",name : "M Apurva",Designation = "student"};
+var user2 = {image : "recentimage.jpeg",name : "Appu",Designation = "student"};
+var isuser1 = true;
+var displayuser;
+var mychildpic = function(){
+    if(isuser1){
+        displayuser = user2;
+        isuser1 = false;
+    }
+    else{
+        displayuser = user1;
+        isuser1 = true;
+    }
+    document.getElementById("myImage").src=displayuser.image;
+    document.getElementById("myname").src=displayuser.name;
+    document.getElementById("myDesig").src=displayuser.desig;
+    
+}
